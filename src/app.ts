a1ffe065a54d1723c3a9bec3a13c2861d6714720
Josh Beard <josh@jlbeard.com>
2020-05-22 15:40:38 -0400

    Added loading indicator

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import { Router, RouterConfiguration } from 'aurelia-router';
 import { PLATFORM } from 'aurelia-pal';
+import { WebAPI } from './web-api';
 
 export class App {
   public router: Router;
+
+  constructor(
+    public api: WebAPI) {
+  }
 
   public configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Contacts';
